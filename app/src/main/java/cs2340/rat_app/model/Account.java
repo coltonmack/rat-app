7761ce463b02c67215dc95b5517b607e9d50f6ec
Borja Vicinay <bvicinay@gmail.com>
2017-09-24 17:33:17 -0400

    Add Account class to model

---
+++ app/src/main/java/cs2340/rat_app/model/Account.java
@@ -0,0 +1,24 @@
+package cs2340.rat_app.model;
+
+/**
+ * Created by Borja Vicinay on 9/24/2017.
+ */
+
+public class Account {
+    //Credentials
+    private String username;
+    private String password;
+
+    //User Data
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Account(String username, String password, String firstName,
+                   String lastName, String email) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+}
