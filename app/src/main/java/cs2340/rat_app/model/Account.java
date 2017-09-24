72fcea6849f2552491908fd6c20223d525f48fa5
tbelford <thomasbelford32@gmail.com>
2017-09-24 17:40:26 -0400

    took out email

--- app/src/main/java/cs2340/rat_app/model/Account.java
+++ app/src/main/java/cs2340/rat_app/model/Account.java
@@ -12,13 +12,14 @@ public class Account {
     //User Data
     private String firstName;
     private String lastName;
-    private String email;
+    //private String email;
 
     public Account(String username, String password, String firstName,
-                   String lastName, String email) {
+                   String lastName) {
         this.username = username;
         this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.email = email;
+        //this.email = email;
+    }
 }
